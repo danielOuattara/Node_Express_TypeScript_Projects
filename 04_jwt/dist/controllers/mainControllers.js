e09c316c0ffe55f5d20bca3d05d6f24ba4515e28
danielOuattara <daniel.ouattara@gmx.com>
2024-01-16 16:46:47 +0100

    04 jwt:  Validate Username and Password

--- 04_jwt/dist/controllers/mainControllers.js
+++ 04_jwt/dist/controllers/mainControllers.js
@@ -8,9 +8,18 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.dashboard = exports.login = void 0;
-const login = (_req, res) => __awaiter(void 0, void 0, void 0, function* () {
+const custom_error_1 = __importDefault(require("../errors/custom-error"));
+const login = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const { username, password } = req.body;
+    console.log(username, password);
+    if (!username || !password) {
+        throw new custom_error_1.default("Please, provide username AND password", 400);
+    }
     return res.status(200).json({ msg: "Fake Login/register/signup routes" });
 });
 exports.login = login;
--- 04_jwt/src/controllers/mainControllers.ts
+++ 04_jwt/src/controllers/mainControllers.ts
@@ -1,7 +1,16 @@
 import { RequestHandler } from "express";
+import CustomAPIError from "../errors/custom-error";
 
 //---------------------------------------------------------------------------
-const login: RequestHandler = async (_req, res) => {
+
+
+const login: RequestHandler = async (req, res) => {
+  const { username, password } = req.body
+  console.log(username, password)
+    /* validations : - mongoose - Joi - controller check */
+  if (!username || !password) {
+    throw new CustomAPIError("Please, provide username AND password", 400);
+  }
   return res.status(200).json({ msg: "Fake Login/register/signup routes" });
 };
 
@@ -15,4 +24,8 @@ const dashboard: RequestHandler = async (_req, res) => {
     });
 };
 
+
+
+
+//----------------------------------------------------------------------------
 export { login, dashboard };
