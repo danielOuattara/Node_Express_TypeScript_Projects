2a3e5b75d7d0ac83233fb85a2889325bb744664c
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-01-26 06:42:03 +0100

    05 jobs_API:backend: userSchema

--- 05_jobs_API/backend/dist/models/UserModel.js
+++ 05_jobs_API/backend/dist/models/UserModel.js
@@ -1,1 +1,27 @@
 "use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const mongoose_1 = require("mongoose");
+const UserSchema = new mongoose_1.Schema({
+    name: {
+        type: String,
+        required: [true, "Name is required. Please provide a name"],
+        trim: true,
+        minLength: 2,
+        maxLength: 50,
+    },
+    email: {
+        type: String,
+        required: [true, "Email is required. Please provide a name"],
+        match: [
+            /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+            "Please provide a valid email",
+        ],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide password"],
+        minLength: 6,
+    },
+});
+exports.default = (0, mongoose_1.model)("User", UserSchema);
---
+++ 05_jobs_API/backend/src/models/UserModel.ts
@@ -0,0 +1,70 @@
+// ---> Separate document interface definition
+
+import { Schema, model } from "mongoose";
+
+interface InterfaceUser {
+  name: string;
+  email: string;
+  password: string;
+}
+
+const UserSchema = new Schema<InterfaceUser>({
+  name: {
+    type: String,
+    required: [true, "Name is required. Please provide a name"],
+    trim: true,
+    minLength: 2,
+    maxLength: 50,
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required. Please provide a name"],
+    match: [
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+      "Please provide a valid email",
+    ],
+    unique: true,
+  },
+    password: {
+    type: String,
+    required: [true, "Please provide password"],
+    minLength: 6,
+  },
+});
+
+
+export default model<InterfaceUser>("User", UserSchema)
+
+//--------------------------------------------------------
+
+// // ---> Automatic type inference
+
+// import { Schema, model, InferSchemaType } from "mongoose";
+
+// const UserSchema = new Schema({
+//   name: {
+//     type: String,
+//     required: [true, "Name is required. Please provide a name"],
+//     trim: true,
+//     minLength: 2,
+//     maxLength: 50,
+//   },
+//   email: {
+//     type: String,
+//     required: [true, "Email is required. Please provide a name"],
+//     match: [
+//       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+//       "Please provide a valid email",
+//     ],
+//     unique: true,
+//   },
+//     password: {
+//     type: String,
+//     required: [true, "Please provide password"],
+//     minLength: 6,
+//   },
+// });
+
+// type TypeUser = InferSchemaType<typeof UserSchema>
+
+// export default model<TypeUser>("User", UserSchema)
