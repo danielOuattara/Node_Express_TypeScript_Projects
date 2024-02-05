4c93d508d8791a54d621a9ae39443bce18fdbc19
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-05 21:57:09 +0100

    05 jobs API: logic for Auth middleware

--- 05_jobs_API/backend/src/middlewares/auth.ts
+++ 05_jobs_API/backend/src/middlewares/auth.ts
@@ -1,5 +1,43 @@
-const auth = () => {
-  console.log("Auth middleware !");
+import { RequestHandler } from "express";
+import jwt from "jsonwebtoken";
+import { UnauthenticatedError } from "../errors";
+import User from "../models/UserModel";
+
+interface ITokenPayload {
+  userId: string;
+  username: string;
+}
+
+const auth: RequestHandler = async (req, _res, next) => {
+  if (
+    !req.headers.authorization ||
+    !req.headers.authorization.startsWith("Bearer")
+  ) {
+    throw new UnauthenticatedError("No token provided");
+  }
+
+  try {
+    const token = req.headers.authorization.split(" ")[1];
+
+    const decoded = jwt.verify(
+      token,
+      process.env.JWT_SECRET as string,
+    ) as ITokenPayload;
+
+    /*--> Less: created local user object: only userId & name*/
+    // req.user = { userId: decoded.userId, username: decoded.username };
+
+    /* --> Better: register in "req" a complete Mongoose user object*/
+    //             with all possible associations.
+    const user = await User.findById(decoded.userId).select("-password");
+    
+    if (user) {
+      req.user = user;
+    }
+    next();
+  } catch (error) {
+    throw new UnauthenticatedError("No token provided");
+  }
 };
 
 export default auth;
