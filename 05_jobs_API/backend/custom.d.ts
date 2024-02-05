487b49304c4637fa42f2950eb1527fe6f9dc8619
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-05 22:00:08 +0100

    05 jobs API: add/ update type for login

---
+++ 05_jobs_API/backend/custom.d.ts
@@ -0,0 +1,92 @@
+// --> good for simple user types definition
+
+// declare namespace Express {
+//   interface Request {
+//     user?: {
+//       userId: string;
+//       username: string;
+//     };
+//   }
+// }
+
+//----------------------------------------------------------------
+
+//--> good for mongoose complete user types definition
+
+// import { Types, Document } from "mongoose";
+// import { Request } from "express";
+
+// interface IUserMethods {
+//   getName(): string;
+//   createJWT(): string;
+//   comparePassword(pwd: string): Promise<boolean>;
+// }
+
+// type MongooseUser =
+//   | (Document<
+//       unknown,
+//       {},
+//       {
+//         name: string;
+//         email: string;
+//         password: string;
+//       }
+//     > &
+//       Omit<
+//         {
+//           name: string;
+//           email: string;
+//           password: string;
+//         } & {
+//           _id: Types.ObjectId;
+//         },
+//         keyof IUserMethods
+//       > &
+//       IUserMethods)
+//   | null;
+
+// declare module "express-serve-static-core" {
+//   interface Request {
+//     user?: MongooseUser;
+//   }
+// }
+
+
+
+import { Types, Document } from "mongoose";
+import { Request } from "express";
+
+interface IUserMethods {
+  getName(): string;
+  createJWT(): string;
+  comparePassword(pwd: string): Promise<boolean>;
+}
+
+type MongooseUser =
+  | (Document<
+      unknown,
+      {},
+      {
+        name: string;
+        email: string;
+        password: string;
+      }
+    > &
+      Omit<
+        {
+          name: string;
+          email: string;
+          password: string;
+        } & {
+          _id: Types.ObjectId;
+        },
+        keyof IUserMethods
+      > &
+      IUserMethods)
+  | null;
+
+declare module "express-serve-static-core" {
+  interface Request {
+    user?: MongooseUser;
+  }
+}
--- 05_jobs_API/backend/type.d.ts
+++ 05_jobs_API/backend/type.d.ts
@@ -1,1 +1,2 @@
 declare module "express-async-errors" 
+
