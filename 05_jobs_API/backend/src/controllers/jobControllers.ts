cf36000cad4222ebd4fa073f72ff4d27585bba4a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-05 21:56:41 +0100

    05 jobs API: jobController: complete CRUD logic

--- 05_jobs_API/backend/src/controllers/jobControllers.ts
+++ 05_jobs_API/backend/src/controllers/jobControllers.ts
@@ -1,28 +1,67 @@
 import { RequestHandler } from "express";
+import Job from "../models/JobModel";
+import { StatusCodes } from "http-status-codes";
+import { BadRequestError, NotFoundError } from "../errors";
 
 //------------------------------------------------------------
-const getAllJobs: RequestHandler = async (_req, res) => {
-  res.send("All jobs");
+const getAllJobs: RequestHandler = async (req, res) => {
+  const jobs = await Job.find({ createdBy: req.user?._id }).sort("createdAt");
+  res.status(StatusCodes.OK).json({ count: jobs.length, jobs });
 };
 
 //------------------------------------------------------------
-const getJob: RequestHandler = async (_req, res) => {
-  res.send(" One job");
+const getJob: RequestHandler = async (req, res) => {
+  const job = await Job.findOne({
+    _id: req.params.jobId,
+    createdBy: req.user!._id,
+  });
+  if (!job) {
+    throw new NotFoundError("Job Not Found");
+  }
+
+  res.status(StatusCodes.OK).json(job);
 };
 
 //------------------------------------------------------------
-const createJob: RequestHandler = async (_req, res) => {
-  res.send("create job");
+const createJob: RequestHandler = async (req, res) => {
+  req.body.createdBy = req.user?._id;
+  const job = await Job.create(req.body);
+  res.status(StatusCodes.CREATED).json(job);
 };
 
 //------------------------------------------------------------
-const patchJob: RequestHandler = async (_req, res) => {
-  res.send("update job");
+const patchJob: RequestHandler = async (req, res) => {
+  if (req.body.company === "" || req.body.position === "") {
+    throw new BadRequestError(
+      "Pease, provide Company and Position for update.",
+    );
+  }
+
+  const job = await Job.findOneAndUpdate(
+    { _id: req.params.jobId, createdBy: req.user?._id },
+    req.body,
+    { new: true, runValidators: true },
+  );
+  if (!job) {
+    throw new NotFoundError(
+      `No job found with for ${req.user!.name} with id ${req.params.jobId}`,
+    );
+  }
+  res.status(StatusCodes.OK).send({ job });
 };
 
 //------------------------------------------------------------
-const deleteJob: RequestHandler = async (_req, res) => {
-  res.send("delete job");
+const deleteJob: RequestHandler = async (req, res) => {
+  const job = await Job.findOneAndDelete({
+    _id: req.params.jobId,
+    createdBy: req.user!._id,
+  });
+  if (!job) {
+    throw new NotFoundError(
+      `No job found with for ${req.user!.name} with id ${req.params.jobId}`,
+    );
+  }
+  res.status(StatusCodes.OK).json({ msg: "job deleted ! " });
 };
 
 export { getAllJobs, getJob, createJob, patchJob, deleteJob };
