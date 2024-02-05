9b6272e4dee307ab41fe806d53d410c97f794c3b
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-05 21:56:04 +0100

    05 jobs API: add login

--- 05_jobs_API/backend/src/controllers/authControllers.ts
+++ 05_jobs_API/backend/src/controllers/authControllers.ts
@@ -1,7 +1,7 @@
 import { RequestHandler } from "express";
 import User from "./../models/UserModel";
 import { StatusCodes } from "http-status-codes";
-// import { BadRequestError } from "./../errors";
+import { BadRequestError, UnauthenticatedError } from "./../errors";
 //------------------------------------------------------------
 const register: RequestHandler = async (req, res) => {
   const user = await User.create(req.body);
@@ -11,11 +11,27 @@ const register: RequestHandler = async (req, res) => {
 };
 
 //------------------------------------------------------------
-const login: RequestHandler = async (_req, res) => {
-  // if (!req.body.name || !req.body.email || !req.body.password) {
-  //   throw new BadRequestError("Please provide: name, email, password");
-  // }
-  res.send("login user");
+const login: RequestHandler = async (req, res) => {
+  // check credentials !
+  if (!req.body.email || !req.body.password) {
+    throw new BadRequestError("Please provide: email and password");
+  }
+  // check user exists !
+  const user = await User.findOne({ email: req.body.email });
+  if (!user) {
+    throw new UnauthenticatedError("User unknown!");
+  }
+
+  // compare password
+  const validPassword = await user.comparePassword(req.body.password);
+  if (!validPassword) {
+    throw new UnauthenticatedError("User unknown!");
+  }
+
+  //All is OK: send token
+  res
+    .status(StatusCodes.OK)
+    .json({ user: { name: user.getName() }, token: user.createJWT() });
 };
 
 export { register, login };
