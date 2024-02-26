dd8f1c7a16847ec1c76f6059ab0f13e7937d0a80
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-26 23:57:16 +0100

    05 Job APi:  security packages

--- 05_jobs_API/backend/dist/app.js
+++ 05_jobs_API/backend/dist/app.js
@@ -8,7 +8,18 @@ require("express-async-errors");
 const middlewares_1 = require("./middlewares");
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const jobRoutes_1 = __importDefault(require("./routes/jobRoutes"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+const express_rate_limit_1 = __importDefault(require("express-rate-limit"));
 const app = (0, express_1.default)();
+app.set("trust proxy", 1);
+app.use((0, express_rate_limit_1.default)({
+    windowMs: 1000,
+    max: 20,
+    message: { code: 429, message: "Too many connection; Try later !" },
+}));
+app.use((0, helmet_1.default)());
+app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/", function (_req, res) {
     res.send("Welcome to Jobs API !");
--- 05_jobs_API/backend/src/app.ts
+++ 05_jobs_API/backend/src/app.ts
@@ -3,10 +3,26 @@ import "express-async-errors";
 import { notFound, errorHandler, auth } from "./middlewares";
 import authRouter from "./routes/authRoutes";
 import jobsRouter from "./routes/jobRoutes";
+import helmet from "helmet";
+import cors from "cors";
+import xss from "xss";
+import rateLimiter from "express-rate-limit";
 
 const app = express();
 
+// security packages
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 1000,
+    max: 20,
+    message: { code: 429, message: "Too many connection; Try later !" },
+  }),
+);
+app.use(helmet());
+app.use(cors());
 app.use(express.json());
+// app.use(xss());
 
 app.get("/", function (_req, res) {
   res.send("Welcome to Jobs API !");
