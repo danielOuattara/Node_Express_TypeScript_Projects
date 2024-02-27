281b33f96bc011ebfca23787831a8ecb2206f834
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-27 18:15:04 +0100

    06 Jobster APi:  cleaning ( according to README.md)

--- 06_jobster_API_extra_project/backend/dist/app.js
+++ 06_jobster_API_extra_project/backend/dist/app.js
@@ -4,8 +4,6 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 const helmet_1 = __importDefault(require("helmet"));
-const cors_1 = __importDefault(require("cors"));
-const express_rate_limit_1 = require("express-rate-limit");
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 const middlewares_1 = require("./middlewares");
@@ -15,14 +13,8 @@ const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
 const yamljs_1 = __importDefault(require("yamljs"));
 const app = (0, express_1.default)();
 app.set("trust proxy", 1);
-app.use((0, express_rate_limit_1.rateLimit)({
-    windowMs: 1000,
-    max: 20,
-    message: { code: 429, message: "Too many connection; Try later !" },
-}));
 app.disable("x-powered-by");
 app.use((0, helmet_1.default)());
-app.use((0, cors_1.default)());
 app.use(express_1.default.json());
 app.get("/", function (_req, res) {
     res.send("<h1>Welcome to Jobs API !</h1> <p>API documentation is available. <a href='/api/v1/docs'>see docs</a></p>");
