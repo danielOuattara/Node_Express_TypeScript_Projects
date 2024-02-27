41da0cb0fc43ee615dab64f794adca9466f6b0d1
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-27 22:21:05 +0100

    06 Jobster APi:  backend, app.ts : clean + update logic + link frontend

--- 06_jobster_API_extra_project/backend/dist/app.js
+++ 06_jobster_API_extra_project/backend/dist/app.js
@@ -9,20 +9,17 @@ require("express-async-errors");
 const middlewares_1 = require("./middlewares");
 const authRoutes_1 = __importDefault(require("./routes/authRoutes"));
 const jobRoutes_1 = __importDefault(require("./routes/jobRoutes"));
-const swagger_ui_express_1 = __importDefault(require("swagger-ui-express"));
-const yamljs_1 = __importDefault(require("yamljs"));
+const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
-app.set("trust proxy", 1);
+app.use(express_1.default.static(path_1.default.resolve(__dirname, "../..", "./frontend/build")));
 app.disable("x-powered-by");
 app.use((0, helmet_1.default)());
 app.use(express_1.default.json());
-app.get("/", function (_req, res) {
-    res.send("<h1>Welcome to Jobs API !</h1> <p>API documentation is available. <a href='/api/v1/docs'>see docs</a></p>");
-});
-const swaggerDocument = yamljs_1.default.load("./swagger-render.yaml");
-app.use("/api/v1/docs", swagger_ui_express_1.default.serve, swagger_ui_express_1.default.setup(swaggerDocument));
 app.use("/api/v1/auth", authRoutes_1.default);
 app.use("/api/v1/jobs", middlewares_1.auth, jobRoutes_1.default);
+app.get("*", (_req, res) => {
+    res.sendFile(path_1.default.resolve(__dirname, "../..", "./frontend/build", "index.html"));
+});
 app.use(middlewares_1.notFound);
 app.use(middlewares_1.errorHandler);
 exports.default = app;
--- 06_jobster_API_extra_project/backend/src/app.ts
+++ 06_jobster_API_extra_project/backend/src/app.ts
@@ -4,28 +4,23 @@ import "express-async-errors";
 import { notFound, errorHandler, auth } from "./middlewares";
 import authRouter from "./routes/authRoutes";
 import jobsRouter from "./routes/jobRoutes";
-
-import swaggerUI from "swagger-ui-express";
-import YAML from "yamljs";
+import path from "path";
 
 const app = express();
-
-app.set("trust proxy", 1);
+app.use(express.static(path.resolve(__dirname, "../..", "./frontend/build")));
 app.disable("x-powered-by");
 app.use(helmet());
 app.use(express.json());
-app.get("/", function (_req, res) {
-  res.send(
-    "<h1>Welcome to Jobs API !</h1> <p>API documentation is available. <a href='/api/v1/docs'>see docs</a></p>",
-  );
-});
-
-// const swaggerDocument = YAML.load("./swagger.yaml");
-const swaggerDocument = YAML.load("./swagger-render.yaml");
-app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
+
+// serve index.html
+app.get("*", (_req, res) => {
+  res.sendFile(
+    path.resolve(__dirname, "../..", "./frontend/build", "index.html"),
+  );
+});
 
 app.use(notFound);
 app.use(errorHandler);
