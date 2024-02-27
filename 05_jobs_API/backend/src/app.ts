0cf35a58209da9dcb85d718b0aa1798b50c9cc00
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-02-27 13:25:27 +0100

    05 Job APi: added YAML + swaggerUI on app.ts

--- 05_jobs_API/backend/src/app.ts
+++ 05_jobs_API/backend/src/app.ts
@@ -1,32 +1,44 @@
+// security imports
+import helmet from "helmet";
+import cors from "cors";
+import { rateLimit } from "express-rate-limit";
+
+// regular imports
 import express from "express";
 import "express-async-errors";
 import { notFound, errorHandler, auth } from "./middlewares";
 import authRouter from "./routes/authRoutes";
 import jobsRouter from "./routes/jobRoutes";
-import helmet from "helmet";
-import cors from "cors";
-import xss from "xss";
-import rateLimiter from "express-rate-limit";
+
+// Swagger + YAML imports
+import swaggerUI from "swagger-ui-express";
+import YAML from "yamljs";
 
 const app = express();
 
 // security packages
 app.set("trust proxy", 1);
 app.use(
-  rateLimiter({
+  rateLimit({
     windowMs: 1000,
     max: 20,
     message: { code: 429, message: "Too many connection; Try later !" },
   }),
 );
+app.disable("x-powered-by");
 app.use(helmet());
 app.use(cors());
 app.use(express.json());
 // app.use(xss());
 
-app.get("/", function (_req, res) {
-  res.send("Welcome to Jobs API !");
+app.get("/api/v1", function (_req, res) {
+  res.send(
+    "<h1>Welcome to Jobs API !</h1> <p>API documentation is available. <a href='/api/v1/docs'>see docs</a></p>",
+  );
 });
+
+const swaggerDocument = YAML.load("./swagger.yaml");
+app.use("/api/v1/docs", swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/jobs", auth, jobsRouter);
