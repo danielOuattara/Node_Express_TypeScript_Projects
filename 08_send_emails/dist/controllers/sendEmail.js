79f0be1d3a1bc9e3fbcdd90b18ae774142946b4d
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-01 18:29:17 +0100

    08 Sending Email: sending email using 'ethereal'

--- 08_send_emails/dist/controllers/sendEmail.js
+++ 08_send_emails/dist/controllers/sendEmail.js
@@ -1,7 +1,36 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sendEmail = void 0;
-const sendEmail = (req, res, next) => {
-    res.send("Sending email");
-};
+const nodemailer_1 = __importDefault(require("nodemailer"));
+const sendEmail = (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    const transporter = nodemailer_1.default.createTransport({
+        host: "smtp.ethereal.email",
+        port: 587,
+        auth: {
+            user: process.env.USER_ACCOUNT,
+            pass: process.env.PASS_ACCOUNT,
+        },
+    });
+    let info = yield transporter.sendMail({
+        from: '"Ricatti" <ricatti@gmx.fr>',
+        to: "bar@example.com, baz@example.com, idell.aufderhar@ethereal.email",
+        subject: "Hello ✔",
+        html: "<b>Hello world?</b>",
+    });
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer_1.default.getTestMessageUrl(info));
+    res.json({ info });
+});
 exports.sendEmail = sendEmail;
--- 08_send_emails/src/controllers/sendEmail.ts
+++ 08_send_emails/src/controllers/sendEmail.ts
@@ -1,8 +1,31 @@
 import { RequestHandler } from "express";
 import nodemailer from "nodemailer";
 
-const sendEmail: RequestHandler = (req, res, next) => {
-  res.send("Sending email");
+const sendEmail: RequestHandler = async (req, res, next) => {
+  const transporter = nodemailer.createTransport({
+    host: "smtp.ethereal.email",
+    port: 587,
+    auth: {
+      user: process.env.USER_ACCOUNT,
+      pass: process.env.PASS_ACCOUNT,
+    },
+  });
+
+  let info = await transporter.sendMail({
+    from: '"Ricatti" <ricatti@gmx.fr>', // sender address
+    to: "bar@example.com, baz@example.com, idell.aufderhar@ethereal.email", // list of receivers
+    subject: "Hello ✔", // Subject line
+    html: "<b>Hello world?</b>", // html body
+  });
+
+  // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@example.com>
+  console.log("Message sent: %s", info.messageId);
+
+  // Preview only available when sending through an Ethereal account
+  console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+  // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
+
+  res.json({ info });
 };
 
 export { sendEmail };
