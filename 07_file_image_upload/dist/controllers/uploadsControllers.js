65ab850b34a6293b772a599963e0f8c493b97b4c
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-01 12:37:20 +0100

    07 File Image Upload: complete logic for cloudinary upload

--- 07_file_image_upload/dist/controllers/uploadsControllers.js
+++ 07_file_image_upload/dist/controllers/uploadsControllers.js
@@ -8,7 +8,39 @@ var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, ge
         step((generator = generator.apply(thisArg, _arguments || [])).next());
     });
 };
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.uploadProductImage = void 0;
-const uploadProductImage = (req, res) => __awaiter(void 0, void 0, void 0, function* () { });
+const http_status_codes_1 = require("http-status-codes");
+const errors_1 = require("../errors");
+const cloudinary_1 = require("cloudinary");
+const node_fs_1 = __importDefault(require("node:fs"));
+const uploadProductImage = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    if (!req.files) {
+        throw new errors_1.BadRequestError("No File Uploaded");
+    }
+    const productImage = req.files.image;
+    if (Array.isArray(productImage)) {
+        res
+            .status(http_status_codes_1.StatusCodes.BAD_REQUEST)
+            .send("Please send one image per request");
+    }
+    else {
+        if (!productImage.mimetype.startsWith("image")) {
+            throw new errors_1.BadRequestError("Only image can be uploaded");
+        }
+        if (productImage.size > parseInt(process.env.IMAGE_MAX_SIZE)) {
+            throw new errors_1.BadRequestError("Image max size is 1Mb");
+        }
+        const result = yield cloudinary_1.v2.uploader.upload(productImage.tempFilePath, {
+            use_filename: true,
+            folder: "file-upload-john-smilga",
+        });
+        console.log("result = ", result);
+        node_fs_1.default.unlinkSync(productImage.tempFilePath);
+        res.status(http_status_codes_1.StatusCodes.CREATED).json({ image: { src: result.secure_url } });
+    }
+});
 exports.uploadProductImage = uploadProductImage;
--- 07_file_image_upload/src/controllers/uploadsControllers.ts
+++ 07_file_image_upload/src/controllers/uploadsControllers.ts
@@ -48,10 +48,40 @@
 ---------------------------------------------- */
 
 import { RequestHandler } from "express";
-import path from "path";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError } from "../errors";
+import { v2 as cloudinary } from "cloudinary";
+import fs from "node:fs";
 
-const uploadProductImage: RequestHandler = async (req, res) => {};
+const uploadProductImage: RequestHandler = async (req, res) => {
+  if (!req.files) {
+    throw new BadRequestError("No File Uploaded");
+  }
+  const productImage = req.files.image;
+  if (Array.isArray(productImage)) {
+    /* Handle multiple files here */
+    res
+      .status(StatusCodes.BAD_REQUEST)
+      .send("Please send one image per request");
+  } else {
+    /* Handle single file */
+    if (!productImage.mimetype.startsWith("image")) {
+      throw new BadRequestError("Only image can be uploaded");
+    }
+
+    if (productImage.size > parseInt(process.env.IMAGE_MAX_SIZE! as string)) {
+      throw new BadRequestError("Image max size is 1Mb");
+    }
+
+    const result = await cloudinary.uploader.upload(productImage.tempFilePath, {
+      use_filename: true,
+      folder: "file-upload-john-smilga",
+    });
+
+    console.log("result = ", result);
+    fs.unlinkSync(productImage.tempFilePath);
+    res.status(StatusCodes.CREATED).json({ image: { src: result.secure_url } });
+  }
+};
 
 export { uploadProductImage };
