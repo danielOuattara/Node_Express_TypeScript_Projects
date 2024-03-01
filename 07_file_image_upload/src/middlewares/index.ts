c0f566c4ea6b484f2fe6b1120955f61d6accaab0
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-01 09:59:35 +0100

    07 File Image Upload: commented in

--- 07_file_image_upload/src/middlewares/index.ts
+++ 07_file_image_upload/src/middlewares/index.ts
@@ -1,5 +1,5 @@
-import auth from "./auth";
+// import auth from "./auth";
 import errorHandler from "./error-handler";
 import notFound from "./not-found";
 
-export { auth, errorHandler, notFound };
+export { /*  auth, */ errorHandler, notFound };
