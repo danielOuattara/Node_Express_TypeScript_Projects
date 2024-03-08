dfb21806aadecc4b8fc74055361c567fd7502271
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-08 06:11:30 +0100

    10 e-commerce API: morgan logger in file

--- 10_e-commerce_API/dist/app.js
+++ 10_e-commerce_API/dist/app.js
@@ -6,7 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 require("express-async-errors");
 const middlewares_1 = require("./middlewares");
+const morgan_1 = __importDefault(require("morgan"));
+const node_fs_1 = __importDefault(require("node:fs"));
+const node_path_1 = __importDefault(require("node:path"));
 const app = (0, express_1.default)();
+const accessLogStream = node_fs_1.default.createWriteStream(node_path_1.default.join(__dirname, "access.log"), { flags: "a" });
+app.use((0, morgan_1.default)("combined", { stream: accessLogStream }));
 app.use(express_1.default.json());
 app.use("/", (req, res) => {
     res.send("E-commerce API");
--- 10_e-commerce_API/src/app.ts
+++ 10_e-commerce_API/src/app.ts
@@ -1,9 +1,42 @@
 import express from "express";
 import "express-async-errors";
 import { errorHandler, notFound } from "./middlewares";
+import morgan from "morgan";
+import fs from "node:fs";
+import path from "node:path";
 
 const app = express();
 
+// --------------------------------------------- logger
+// create a write stream (in append mode)
+const accessLogStream = fs.createWriteStream(
+  path.join(__dirname, "access.log"),
+  { flags: "a" },
+);
+
+// app.use(morgan("tiny"));
+// app.use(morgan("combined"));
+// app.use(
+//   morgan(function (tokens, req, res) {
+//     return [
+//       tokens.method(req, res),
+//       tokens.url(req, res),
+//       tokens.status(req, res),
+//       tokens.res(req, res, "content-length"),
+//       "-",
+//       tokens["response-time"](req, res),
+//       "ms",
+//     ].join(" ");
+//   }),
+// );
+
+// app.use(
+//   morgan(":method :url :status :res[content-length] - :response-time ms"),
+// );
+
+app.use(morgan("combined", { stream: accessLogStream }));
+
+//----------------------------------------------------
 app.use(express.json());
 
 app.use("/", (req, res) => {
