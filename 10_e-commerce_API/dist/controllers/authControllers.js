901e76060ba90f63e8ecdc28fa620a1c69f1099d
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-03-08 10:44:05 +0100

    10 e-commerce API: register user + check unique email check is working

--- 10_e-commerce_API/dist/controllers/authControllers.js
+++ 10_e-commerce_API/dist/controllers/authControllers.js
@@ -1,9 +1,30 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.logout = exports.login = exports.register = void 0;
-const register = (_req, res) => {
-    res.send("register user");
-};
+const UserModel_1 = __importDefault(require("./../models/UserModel"));
+const http_status_codes_1 = require("http-status-codes");
+const register = (req, res) => __awaiter(void 0, void 0, void 0, function* () {
+    const user = yield UserModel_1.default.create(req.body);
+    res.status(http_status_codes_1.StatusCodes.CREATED).json({
+        user: {
+            name: user.name,
+            email: user.email,
+            password: user.password,
+        },
+    });
+});
 exports.register = register;
 const login = (_req, res) => {
     res.send("login user");
--- 10_e-commerce_API/src/controllers/authControllers.ts
+++ 10_e-commerce_API/src/controllers/authControllers.ts
@@ -1,9 +1,23 @@
 import { RequestHandler } from "express";
+import User from "./../models/UserModel";
+import { StatusCodes } from "http-status-codes";
+import {
+  BadRequestError,
+  NotFoundError,
+  UnauthenticatedError,
+} from "./../errors";
 
 //-----------------------------------------------------
 
-const register: RequestHandler = (_req, res) => {
-  res.send("register user");
+const register: RequestHandler = async (req, res) => {
+  const user = await User.create(req.body);
+  res.status(StatusCodes.CREATED).json({
+    user: {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+    },
+  });
 };
 
 //-----------------------------------------------------
