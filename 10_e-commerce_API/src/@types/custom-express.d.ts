5d2d4812198bd4e9484bf61755f86b10734f8170
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-02 15:49:15 +0200

    10 e-commerce API: create types (Express Module) augmentation + update source types for Express

--- 10_e-commerce_API/src/@types/custom-express.d.ts
+++ 10_e-commerce_API/src/@types/custom-express.d.ts
@@ -3,5 +3,6 @@ import { MongooseUser } from "./user";
 declare module "express-serve-static-core" {
   interface Request {
     user?: MongooseUser;
+    signedCookies: Record<string, string | undefined>;
   }
 }
