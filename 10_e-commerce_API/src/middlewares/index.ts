cafe2ef69a4eefd5d0a300f7c2e424b5937cd00b
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-04-02 15:48:04 +0200

    10 e-commerce API: update middlewares imports sources

--- 10_e-commerce_API/src/middlewares/index.ts
+++ 10_e-commerce_API/src/middlewares/index.ts
@@ -1,5 +1,17 @@
-// import auth from "./auth";
+import {
+  authenticateUser,
+  authenticateAdmin,
+  authenticateRoles,
+} from "./authenticateMiddlewares";
 import { errorHandler } from "./error-handler";
 import { notFound } from "./not-found";
 import { testUser } from "./testUser";
-export { /* auth, */ errorHandler, notFound, testUser };
+
+export {
+  authenticateUser,
+  authenticateAdmin,
+  authenticateRoles,
+  errorHandler,
+  notFound,
+  testUser,
+};
