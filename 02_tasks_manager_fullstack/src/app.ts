dfd4c4aebf9d73a3f4544efd6492f93d5a61255a
Daniel B. OUATTARA <daniel.ouattara@gmx.com>
2024-10-10 14:47:44 +0200

    02: task manager fullstack: add express-rate-limiter on tasks route

--- 02_tasks_manager_fullstack/src/app.ts
+++ 02_tasks_manager_fullstack/src/app.ts
@@ -2,6 +2,15 @@ import express from "express";
 import taskRouter from "./routes/tasksRoutes";
 import { notFound, customErrorHandler } from "./middlewares";
 
+import { rateLimit } from "express-rate-limit";
+
+const jobsLimiter = rateLimit({
+  windowMs: 5 * 60 * 1000,
+  max: 10,
+  limit: 5,
+  message: { code: 429, msg: "Too many connection; Try later in 7 mins !" },
+});
+
 const app = express();
 
 // app.get("/", (_req: Request, res: Response) => {
@@ -11,7 +20,7 @@ const app = express();
 app.use(express.static("./public"));
 app.use(express.json());
 
-app.use("/api/v1/tasks", taskRouter);
+app.use("/api/v1/tasks", jobsLimiter, taskRouter);
 
 app.use(notFound);
 app.use(customErrorHandler);
